81c0c900acc197d85cc36dc6003a523f00edb4fa
CeciEscobar <0247792@up.edu.mx>
2024-08-20 17:56:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit as st
 
 st.title("Mi Curriculum")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
  #Título
 st.title("Cecilia Escobar Briones")
