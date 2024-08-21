52998d76039505447d83f43905339cb3bb01e02c
CeciEscobar <0247792@up.edu.mx>
2024-08-20 18:00:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("Mi Curriculum")
+st.title("Curriculum personal | Cecilia Escobar")
 
  #Título
 st.title("Cecilia Escobar Briones")
+
+DESCRIPTION= """
+Estudiante universitaria de Administración y Finanzas en séptimo semestre con
+experiencia en el área de administración y servicio al cliente. Estoy comprometida con
+mi crecimiento personal y busco poder aplicar lo aprendido en la universidad en un
+entorno profesional y dinámico donde pueda contribuir al exito de su empresa.
+"""
